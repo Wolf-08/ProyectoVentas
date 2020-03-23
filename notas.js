490da11f0a135412dd2c86f00a35fd0290507ca6
Wolf-08 <alejandro.montecillo@gmail.com>
2020-03-22 20:09:29 -0600

    Inicializando proyecto de ventas

---
+++ notas.js
@@ -0,0 +1,8 @@
+// Servidor Web;
+// No copiar y pegar código;
+// Funcionamiento de los paquetes;
+// Nodejs; Digital Ocean 
+// /**Que se puede hacer con node js, Servidores web 
+/** Tambien se pueden hacer bases y conectarse a una Aplicaciones de Escritorio */
+
+// npm init para inicializar un proyecto.
